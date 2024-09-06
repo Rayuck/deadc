1c98bf60c01109bb5c01c45b1386d22a5c7c9411
jayuck <jacktheamazinghd@gmail.com>
2024-09-06 19:32:15 +0100

    main

---
+++ README.md
@@ -0,0 +1,32 @@
+
+# Installation
+
+## install gcc:
+
+### Arch Linux
+
+```sudo pacman -S gcc```
+
+### Debian
+
+```sudo apt-get install gcc```
+
+### Mac
+
+```brew install gcc```
+
+### Windows
+
+Install from the [sourceforge](https://sourceforge.net/projects/gcc-win64/) website
+
+## clone & compile:
+
+```git clone https://github.com/jayuck/deadc```
+
+```cd deadc```
+
+```gcc main.c -o deadc```
+
+## use:
+
+```deadc <path_to_deadfish_file>```
--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("test");
-    printf("test");
+int main(int argc, char *argv[]){
+    FILE *f = fopen(argv[1], "r");
+    if(f==NULL){
+        printf("Invalid File: %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    fseek(f, 0, SEEK_END);
+    int fsize = ftell(f);
+    rewind(f);
+
+    char *filecontent = calloc(fsize, sizeof(char));
+
+    fread(filecontent, sizeof(char), fsize, f);
+    fclose(f);
+
+    char cell=0;
+
+    for (int i=0;i<fsize;i++){
+        switch (filecontent[i]){
+            case 'i': ++cell; break;
+            case 'd': --cell; break;
+            case 's': cell=cell*cell; break;
+            case 'o': printf("%i", cell);
+        }
+    }
+
+    free(filecontent);
+    return EXIT_SUCCESS;
 }
