2511019dc6ad018623204f3d348824e20c2c8cb5
Rayuck <jacktheamazinghd@gmail.com>
2024-12-10 21:56:10 +0000

    Add explanatory comments to main.c
    that's it

--- main.c
+++ main.c
@@ -3,22 +3,28 @@
 
 int main(int argc, char *argv[]){
     FILE *f = fopen(argv[1], "r");
+
+    // error out if file not found
     if(f==NULL){
         printf("Invalid File: %s\n", argv[1]);
         return EXIT_FAILURE;
     }
 
+    // find file size
     fseek(f, 0, SEEK_END);
     int fsize = ftell(f);
     rewind(f);
 
+    // allocate memory dependent on file size
     char *filecontent = calloc(fsize, sizeof(char));
 
+    // read contents of file to filecontent (no shit) 
     fread(filecontent, sizeof(char), fsize, f);
     fclose(f);
 
     char cell=0;
 
+    // general logic
     for (int i=0;i<fsize;i++){
         switch (filecontent[i]){
             case 'i': ++cell; break;
@@ -29,5 +35,7 @@ int main(int argc, char *argv[]){
     }
 
     free(filecontent);
+
+    // HURRAY :)
     return EXIT_SUCCESS;
 }
